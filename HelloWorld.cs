b88d36c4d5051ca15289629695462588618ce0b4
Igor-16 <malnoch.igor@mail.ru>
2025-06-12 19:15:37 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour 
+{
+    private void Start()
+    {
+        //Выводит сообщение
+        print("Hello World!");
+    }
+}
